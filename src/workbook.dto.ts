0c01ea0e009bf0a3f93ff1f95713c61cb0168c1e
Michael Rudolph <michaelrudolph@posteo.net>
2024-12-06 10:29:23 +0100

    feat: Add levels to workbook schema

--- src/workbook.dto.ts
+++ src/workbook.dto.ts
@@ -6,8 +6,16 @@ const GroupSchema = z.object({
   name: z.string(),
 });
 
+const EvaluationLevelSchema = z.object({
+  level: z.number(),
+  name: z.string(),
+  pointsFrom: z.number(),
+  pointsTo: z.number(),
+});
+
 export const WorkbookResponseBodySchema = z.object({
   version: z.nativeEnum(BalanceSheetVersion),
   type: z.nativeEnum(BalanceSheetType),
   groups: GroupSchema.array(),
+  evaluationLevels: EvaluationLevelSchema.array(),
 });
--- tests/workbook.dto.spec.ts
+++ tests/workbook.dto.spec.ts
@@ -10,6 +10,20 @@ describe('Workbook DTO', () => {
         { shortName: 'A', name: 'A name' },
         { shortName: 'B', name: 'B name' },
       ],
+      evaluationLevels: [
+        {
+          level: 0,
+          name: 'Vorbildlich',
+          pointsFrom: 7,
+          pointsTo: 10,
+        },
+        {
+          level: 1,
+          name: 'Erfahren',
+          pointsFrom: 4,
+          pointsTo: 6,
+        },
+      ],
     };
     expect(WorkbookResponseBodySchema.parse(workbook)).toMatchObject(workbook);
   });

2b40bce5139db7081aba11599d1240814d2b7ef9
Michael Rudolph <michaelrudolph@posteo.net>
2025-03-13 16:49:18 +0100

    feat: Add audit submit response dto

--- src/audit.dto.ts
+++ src/audit.dto.ts
@@ -3,3 +3,7 @@ import { z } from 'zod';
 export const AuditSubmitRequestBodySchema = z.object({
   balanceSheetToBeSubmitted: z.number(),
 });
+
+export const AuditSubmitResponseBodySchema = z.object({
+  id: z.number(),
+});
--- tests/audit.dto.spec.ts
+++ tests/audit.dto.spec.ts
@@ -1,4 +1,7 @@
-import { AuditSubmitRequestBodySchema } from '../src/audit.dto';
+import {
+  AuditSubmitRequestBodySchema,
+  AuditSubmitResponseBodySchema,
+} from '../src/audit.dto';
 
 describe('AuditSubmitRequestBodySchema', () => {
   it('parse from json', () => {
@@ -6,6 +9,16 @@ describe('AuditSubmitRequestBodySchema', () => {
       balanceSheetToBeSubmitted: 9,
     };
     const parsed = AuditSubmitRequestBodySchema.parse(json);
+    expect(parsed).toEqual(json);
+  });
+});
+
+describe('AuditSubmitResponseBodySchema', () => {
+  it('parse from json', () => {
+    const json = {
+      id: 10,
+    };
+    const parsed = AuditSubmitResponseBodySchema.parse(json);
     expect(parsed).toEqual(json);
   });
 });
